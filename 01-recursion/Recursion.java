67197610d16faccd3434d5e08bc0e5084ede55fb
i has candys <ichen10@stuy.edu>
2021-02-08 20:33:52 -0500

    implemented countNoDoubleLetterWords in Recursion class

--- 01-recursion/Recursion.java
+++ 01-recursion/Recursion.java
@@ -7,4 +7,21 @@ public class Recursion {
 			return reverse(s.substring(1)) + s.charAt(0);
 		}
 	}
+	public static long countNoDoubleLetterWords(int length, String word) {
+		long sum = 0;
+		if (length == 0) {
+			return 1;
+		}
+		char[] alphabet = new char[26];
+		for (char i = 'a'; i <= 'z'; i++) {
+			alphabet[i-'a'] = i;
+		}
+		for (int i = 0; i < 26; i++) {
+			char letter = alphabet[i];
+			if (word.length() == 0 || word.charAt(word.length()-1) != letter) {
+				sum += countNoDoubleLetterWords(length-1, word + letter);
+			}
+		}
+		return sum;
+	}
 }
--- 01-recursion/Tester.java
+++ 01-recursion/Tester.java
@@ -2,5 +2,6 @@ public class Tester {
 	public static void main(String[] args) {
 		String a = "woc";
 		System.out.println(Recursion.reverse(a) );
+		System.out.println(Recursion.countNoDoubleLetterWords(3, ""));
 	}
 }

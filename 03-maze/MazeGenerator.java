4e43a2643859f4dde61ccff5bede3e8355fad388
i has candys <ichen10@stuy.edu>
2021-03-02 21:41:18 -0500

    fixed variable names in checkAdjacent to resolve compile errors; added chooseDirection helper method

--- 03-maze/MazeGenerator.java
+++ 03-maze/MazeGenerator.java
@@ -2,33 +2,45 @@ import java.util.*;
 import java.io.*;
 public class MazeGenerator {
 	public static void generate(char[][]maze, int rows, int cols, int startrow, int startcol) {
-		int adjCounter = 0;
 		rows = maze.length;
 		cols = maze[0].length;
 	}
 	private static boolean checkAdjacent(char[][] maze, int row, int col) {
-		if (maze[startrow+1][startcol]==' ') {
+		int adjCounter = 0;
+		if (maze[row+1][col]==' ') {
 			adjCounter++;
 		}
-		if (maze[startrow-1][startcol]==' ') {
+		if (maze[row-1][col]==' ') {
 			adjCounter++;
 		}
-		if (maze[startrow][startcol+1]==' ') {
+		if (maze[row][col+1]==' ') {
 			adjCounter++;
 		}
-		if (maze[startrow][startcol-1]==' ') {
+		if (maze[row][col-1]==' ') {
 			adjCounter++;
 		}
 		return adjCounter <= 1;
 	}
-	private static String chooseDirection(char[][] maze, int row, int col) {
+	private static String chooseRandomDirection(char[][] maze, int row, int col) {
 		String[] temp = {"right", "left", "up", "down"};
 		ArrayList<String> directions = new ArrayList<String>(Arrays.asList(temp));
-		if (row <= 1) {
+		if (row <= 1 || !checkAdjacent(maze,row-1,col)) {
 			directions.remove("up");
 		}
-		if (row >= maze.length-2) {
+		if (row >= maze.length-2 || checkAdjacent(maze,row+1,col)) {
 			directions.remove("down");
+		}
+		if (col <= 1 || !checkAdjacent(maze,row,col-1)) {
+			directions.remove("left");
+		}
+		if (col >= maze.length-2 || checkAdjacent(maze,row,col+1)) {
+			directions.remove("right");
+		}
+		if (directions.size() > 0) {
+			return directions.get((int) (directions.size() * Math.random()));
+		}
+		else {
+			return "";
 		}
 	}
 }

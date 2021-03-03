e65934c9f82e075f0a338b0b3af0abdf41c827fe
i has candys <ichen10@stuy.edu>
2021-03-02 21:33:38 -0500

    added helper method checkAdjacent to check if a particular (row, col) is next to two other open squares

--- 03-maze/MazeGenerator.java
+++ 03-maze/MazeGenerator.java
@@ -1,3 +1,34 @@
+import java.util.*;
+import java.io.*;
 public class MazeGenerator {
-	
+	public static void generate(char[][]maze, int rows, int cols, int startrow, int startcol) {
+		int adjCounter = 0;
+		rows = maze.length;
+		cols = maze[0].length;
+	}
+	private static boolean checkAdjacent(char[][] maze, int row, int col) {
+		if (maze[startrow+1][startcol]==' ') {
+			adjCounter++;
+		}
+		if (maze[startrow-1][startcol]==' ') {
+			adjCounter++;
+		}
+		if (maze[startrow][startcol+1]==' ') {
+			adjCounter++;
+		}
+		if (maze[startrow][startcol-1]==' ') {
+			adjCounter++;
+		}
+		return adjCounter <= 1;
+	}
+	private static String chooseDirection(char[][] maze, int row, int col) {
+		String[] temp = {"right", "left", "up", "down"};
+		ArrayList<String> directions = new ArrayList<String>(Arrays.asList(temp));
+		if (row <= 1) {
+			directions.remove("up");
+		}
+		if (row >= maze.length-2) {
+			directions.remove("down");
+		}
+	}
 }

02192cf4a985f6b35c94b62f3a9a62ba72637476
i has candys <ichen10@stuy.edu>
2021-03-08 23:44:30 -0500

    tested and publicized makeArrayBronze

--- 04-usaco/Tester.java
+++ 04-usaco/Tester.java
@@ -1,5 +1,15 @@
+import java.io.*;
+import java.util.Scanner;
+import java.util.Arrays;
 public class Tester {
 	public static void main(String[] args) {
-		
+		String filename = args[0];
+		try {
+			USACO.bronze(filename);
+		}
+		catch (FileNotFoundException e) {
+			System.out.println("File not found: " + filename);
+			System.exit(1);
+		}
 	}
 }
--- 04-usaco/USACO.java
+++ 04-usaco/USACO.java
@@ -2,19 +2,20 @@ import java.io.*;
 import java.util.Scanner;
 import java.util.Arrays;
 public class USACO {
-	public static int bronze(String filename) throws FileNotFoundException{
+	public static int bronze(String filename) throws FileNotFoundException {
 		File f = new File(filename);
 		Scanner s = new Scanner(f);
 		Scanner firstLine = new Scanner(s.nextLine());
-		int rows = firstLine.next();
-		int cols = firstLine.next();
-		int elevation = firstLine.next();
-		int numInstructions = firstLine.next();
+		int rows = Integer.parseInt(firstLine.next());
+		int cols = Integer.parseInt(firstLine.next());
+		int elevation = Integer.parseInt(firstLine.next());
+		int numInstructions = Integer.parseInt(firstLine.next());
 
 		int[][] grid = makeArrayBronze(rows, cols, s);
+		System.out.println(Arrays.deepToString(grid));
 		return -1;
 	}
-	private static int[][] makeArrayBronze(int rows, int cols, Scanner s) {
+	public static int[][] makeArrayBronze(int rows, int cols, Scanner s) {
 		int[][] retArray = new int[rows][cols];
 		for (int row=0; row < rows; row++) {
 			Scanner line = new Scanner(s.nextLine());

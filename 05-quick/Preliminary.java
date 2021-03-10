91a3319bbbb67a3b09c7da1189d78401dfd636a9
i has candys <ichen10@stuy.edu>
2021-03-10 15:53:36 -0500

    added private swap method

--- 05-quick/Preliminary.java
+++ 05-quick/Preliminary.java
@@ -1,3 +1,24 @@
 public class Preliminary {
-	
+	public static int partition (int[] data, int start, int end) {
+		int randomIndex = start + (int) Math.random(end-start);
+		swap(data, start, randomIndex);
+		int pivot = data[start];
+
+		int si = start+1;
+		int ei = end;
+
+		while (si >= ei) {
+			if (data[si] < pivot) {
+				si++;
+			}
+			else if (data[si] > pivot) {
+				
+			}
+		}
+	}
+	private static void swap(int[] data, int i1, int i2) {
+		int temp = data[i1];
+		data[i1] = data[i2];
+		data[i2] = temp;
+	}
 }

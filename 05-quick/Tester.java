f6dc478dca4aa066001561ec0e65a4734d31fa19
i has candys <ichen10@stuy.edu>
2021-03-10 16:10:53 -0500

    tested partition where start = end

--- 05-quick/Tester.java
+++ 05-quick/Tester.java
@@ -4,5 +4,8 @@ public class Tester {
 		int[] test1 = {999,999,999,4,3,2,1,0,999,999,999};
 		System.out.println(Preliminary.partition(test1, 3, 7));
 		System.out.println(Arrays.toString(test1));
+		int[] test2 = {999,999,999,4,3,2,1,0,999,999,999};
+		System.out.println(Preliminary.partition(test2, 5, 5));
+		System.out.println(Arrays.toString(test2));
 	}
 }

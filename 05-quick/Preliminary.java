393688f78a15e8477b49ddcbd15d5486eee489c7
i has candys <ichen10@stuy.edu>
2021-03-10 16:00:40 -0500

    fully implemented partition; haven't yet tested

--- 05-quick/Preliminary.java
+++ 05-quick/Preliminary.java
@@ -1,6 +1,6 @@
 public class Preliminary {
 	public static int partition (int[] data, int start, int end) {
-		int randomIndex = start + (int) Math.random(end-start);
+		int randomIndex = start + (int) ( Math.random()*(end-start));
 		swap(data, start, randomIndex);
 		int pivot = data[start];
 
@@ -12,8 +12,20 @@ public class Preliminary {
 				si++;
 			}
 			else if (data[si] > pivot) {
-				
+				swap(data, si, ei);
+				ei--;
 			}
+			else if (Math.random() < 0.5) {
+				swap(data, si, ei);
+			}
+		}
+		if (data[si] < pivot) {
+			swap(data, si, start);
+			return si;
+		}
+		else {
+			swap(data, si-1, start);
+			return si-1;
 		}
 	}
 	private static void swap(int[] data, int i1, int i2) {

e5565db73822b02ba6cdc0ec60f017b3e44248ad
i has candys <ichen10@stuy.edu>
2021-03-18 22:03:30 -0400

    added some code for merge method, haven't yet tested

--- 06-merge/Merge.java
+++ 06-merge/Merge.java
@@ -1,5 +1,33 @@
 public class Merge {
-	public static void mergesort(int[]data) {
+	public static void mergesort(int[] data) {
 		System.out.println("CODE THIS");
 	}
+	public static void merge(int[] source, int[] dest, int sStart, int sEnd, int dStart, int dEnd) {
+		int si = sStart;
+		int di = dStart;
+		int i=sStart;
+		while (si < sEnd && di < dEnd) {
+			if (source[si] < dest[di]) {
+				dest[i] = source[si];
+				si++;
+				i++;
+			}
+			else {
+				dest[i] = dest[di];
+				di++;
+				i++;
+			}
+		}
+		if (si < sEnd) {
+			for (si; si<sEnd; si++) {
+				dest[i] = source[si];
+				i++;
+			}
+		}
+		else {
+			for (di; di<dEnd; di++) {
+				dest[i] = dest[di];
+				i++;
+			}
+		}
 }

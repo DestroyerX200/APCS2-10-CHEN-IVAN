9d6a56c1089e4caf35c0b81f4cd1deba4422b5c3
i has candys <ichen10@stuy.edu>
2021-03-18 22:18:47 -0400

    fixed indexing bugs in merge and tested

--- 06-merge/Merge.java
+++ 06-merge/Merge.java
@@ -2,32 +2,32 @@ public class Merge {
 	public static void mergesort(int[] data) {
 		System.out.println("CODE THIS");
 	}
-	public static void merge(int[] source, int[] dest, int sStart, int sEnd, int dStart, int dEnd) {
-		int si = sStart;
-		int di = dStart;
-		int i=sStart;
-		while (si < sEnd && di < dEnd) {
-			if (source[si] < dest[di]) {
-				dest[i] = source[si];
-				si++;
+	public static void merge(int[] source, int[] dest, int start, int middle, int end) {
+		int i = start;
+		int j = middle+1;
+		int k = start;
+		while (i <= middle && j <= end) {
+			if (source[i] < source[j]) {
+				dest[k] = source[i];
 				i++;
 			}
 			else {
-				dest[i] = dest[di];
-				di++;
-				i++;
+				dest[k] = source[j];
+				j++;
 			}
+			k++;
 		}
-		if (si < sEnd) {
-			for (si; si<sEnd; si++) {
-				dest[i] = source[si];
-				i++;
+		if (i <= middle) {
+			for (;i <= middle; i++) {
+				dest[k] = source[i];
+				k++;
 			}
 		}
 		else {
-			for (di; di<dEnd; di++) {
-				dest[i] = dest[di];
-				i++;
+			for (;j <= end; j++) {
+				dest[k] = source[j];
+				k++;
 			}
 		}
+	}
 }
--- 06-merge/Tester.java
+++ 06-merge/Tester.java
@@ -1,3 +1,9 @@
+import java.util.Arrays;
 public class Tester {
-	
+	public static void main(String[] args) {
+		int[] source = {1,2,6,9,10,3,5,8,9};
+		int[] dest = {1,2,6,9,10,3,5,8,9};
+		Merge.merge(source, dest, 0, 4, 8);
+		System.out.println(Arrays.toString(dest));
+	}
 }

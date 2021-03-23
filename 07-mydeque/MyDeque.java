f63e71642cd99f0b077e803f4d077a419b30bf22
i has candys <ichen10@stuy.edu>
2021-03-23 00:55:44 -0400

    implemented toString() and size() methods, and changed start,end indices in constructors

--- 07-mydeque/MyDeque.java
+++ 07-mydeque/MyDeque.java
@@ -7,15 +7,41 @@ public class MyDeque<E> {
     E[] d = (E[])new Object[10];
     data = d;
     size = 0;
-    start = 9;
-    end = 0;
+    start = 10;
+    end = -1;
 	}
+
 	public MyDeque(int initialCapacity) {
-		@SuppressWarnings("unchecked")
-		E[] d = (E[])new Object[initialCapacity];
-    data = d;
-    size = 0;
-    start = initialCapacity-1;
-    end = 0;
+		if (initialCapacity < 10) {
+			this();
+		}
+		else {
+			@SuppressWarnings("unchecked")
+			E[] d = (E[])new Object[initialCapacity];
+	    data = d;
+	    size = 0;
+	    start = initialCapacity;
+	    end = -1;
+	  }
+	}
+
+	public int size() {
+		return size;
+	}
+
+	public String toString() {
+		if (size==0) {
+			return "{}";
+		}
+		String retStr ="{";
+		for (int i=start; i<size; i++) {
+			retStr+= data[i] + ", ";
+		}
+		for (int j=0; j<=end; j++) {
+			retStr+= data[j] + ", ";
+		}
+		retStr = retStr.substring(0,retStr.length()-2);
+		retStr += "}";
+		return retStr;
 	}
 }

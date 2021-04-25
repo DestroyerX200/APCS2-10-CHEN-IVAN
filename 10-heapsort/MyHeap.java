3758cbe74d6b4b674ee362d1520c419a0da87e85
DestroyerX200 <ichen10@stuy.edu>
2021-04-25 19:03:27 -0400

    implemented helper method swap and pushDown() recursively; haven't yet tested

--- 10-heapsort/MyHeap.java
+++ 10-heapsort/MyHeap.java
@@ -1,3 +1,21 @@
 public class MyHeap {
-	
+	private static void pushDown(int[] data, int size, int index) {
+		if (2*index+1 < size) {
+			int childIndex = 2*index+1;
+			int maxChild = data[2*index+1];
+			if (2*index+2 < size && maxChild < data[2*index+2]) {
+				childIndex++;
+				maxChild = data[childIndex];
+			}
+			if (maxChild > data[index]) {
+				swap(data, index, childIndex);
+				pushDown(data, size, childIndex);
+			}
+		}
+	}
+	private static void swap(int[] data, int i1, int i2) {
+		int temp = data[i1];
+		data[i1] = data[i2];
+		data[i2] = temp;
+	}
 }

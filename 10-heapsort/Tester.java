69b3ea4dd9b16885ef0559c709754f4060d9b5e6
DestroyerX200 <ichen10@stuy.edu>
2021-04-26 19:00:17 -0400

    more extensive testing done; heapsort seems to work properly

--- 10-heapsort/Tester.java
+++ 10-heapsort/Tester.java
@@ -1,26 +1,32 @@
 import java.util.Arrays;
 public class Tester {
 	public static void main(String[] args) {
-		// int size = 10000;
-		// double numTests = 1000;
-		// double passedTests=0;
-		// for (double i=0; i < numTests; i++) {
-		// 	int[] test = makeArray(size);
-		// 	MyHeap.buildHeap(test);
-		// 	if (isHeap(test)) {
-		// 		passedTests++;
-		// 	}
-		// }
-		// System.out.println(100*numTests/passedTests + "% of tests passed!");
+		int size = 100000;
+		double numTests = 1000;
+		double passedTests=0;
+		if (args.length > 0) {
+			size = Integer.parseInt(args[0]);
+		}
+		if (args.length > 1) {
+			numTests = Double.parseDouble(args[1]);
+		}
+		for (double i = 0; i < numTests; i++) {
+			int[] test = makeArray(size);
+			MyHeap.heapsort(test);
+			if (isSorted(test)) {
+				passedTests++;
+			}
+		}
+		System.out.println(100*numTests/passedTests + "% of tests passed!");
 
-		int[] testSort1 = makeArray(20);
-		System.out.println(Arrays.toString(testSort1));
-		MyHeap.heapsort(testSort1);
-		System.out.println(Arrays.toString(testSort1));
+		// int[] testSort1 = makeArray(20);
+		// System.out.println(Arrays.toString(testSort1));
+		// MyHeap.heapsort(testSort1);
+		// System.out.println(Arrays.toString(testSort1));
 
 	}
 	public static boolean isHeap(int[] data) {
-		for (int i=0; i<data.length; i++) {
+		for (int i = 0; i < data.length; i++) {
 			if (2*i+1 < data.length && data[2*i+1] > data[i]) {
 				return false;
 			}
@@ -32,9 +38,17 @@ public class Tester {
 	}
 	public static int[] makeArray(int size) {
 		int[] retArray = new int[size];
-		for(int i=0; i<size; i++) {
+		for(int i = 0; i < size; i++) {
 			retArray[i] = (int) (10000*Math.random());
 		}
 		return retArray;
+	}
+	public static boolean isSorted(int[] data) {
+		for (int i = 0; i < data.length-1; i++) {
+			if (data[i+1] < data[i]) {
+				return false;
+			}
+		}
+		return true;
 	}
 }
